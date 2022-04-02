098b10731fdd92b1ed2843e8cfb528e4b7cb547b
Tuba Nur <yasatubanur@gmail.com>
2022-04-02 15:16:59 +0300

    getting location coded

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
 
+    @IBOutlet var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.stopUpdatingLocation()
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations[0].coordinate.latitude)
+        print(locations[0].coordinate.longitude)
+    }
 
 }
 
